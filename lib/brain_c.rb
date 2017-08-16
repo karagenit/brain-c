2d9410fc8372b462dea9518369b5dea2b6eb4bb0
Caleb Smith <karagenit@outlook.com>
2017-08-15 22:31:15 -0400

    Add Preprocessing & Parsing

--- lib/brain_c.rb
+++ lib/brain_c.rb
@@ -3,6 +3,16 @@
 #
 module BrainC
   def self.compile(file)
-    file
+    src = preprocess(file)
+    src = get_main_body(src)
+    src
+  end
+
+  def self.preprocess(str)
+    str.delete("\n")
+  end
+
+  def self.get_main_body(str)
+    str[/int main\(\) \{(.*)\}/, 1]
   end
 end
--- test/int.c
+++ test/int.c
@@ -1,3 +1,5 @@
 int main() {
-  int i;
+    int i;
+    i = 3;
+    putchar(i);
 }
