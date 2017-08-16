6d2b547ec28d54c72e7aff226448c6c3fa3aa542
Caleb Smith <karagenit@outlook.com>
2017-08-16 10:46:48 -0400

    Add Variable Class

--- lib/brain_c.rb
+++ lib/brain_c.rb
@@ -15,4 +15,22 @@ module BrainC
   def self.get_main_body(str)
     str[/int main\(\) \{(.*)\}/, 1]
   end
+
+  ##
+  # This class represents a Variable on the BF Tape
+  #
+  class Variable
+    attr_reader :name
+    attr_reader :index
+    attr_accessor :value
+
+    def initialize(name, index, value = nil)
+      raise ArgumentError if name.nil?
+      raise ArgumentError if index < 0
+
+      @name = name
+      @index = index
+      @value = value
+    end
+  end
 end
