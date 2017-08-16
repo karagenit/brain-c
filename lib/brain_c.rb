0ba9c581904094363a3ac646ca80b5eefb1f6bee
Caleb Smith <karagenit@outlook.com>
2017-08-16 10:53:34 -0400

    Add Command Parsing

--- lib/brain_c.rb
+++ lib/brain_c.rb
@@ -2,18 +2,14 @@
 # Compilation tools for converting C to Brainfuck
 #
 module BrainC
-  def self.compile(file)
-    src = preprocess(file)
-    src = get_main_body(src)
-    src
-  end
-
-  def self.preprocess(str)
-    str.delete("\n")
-  end
-
-  def self.get_main_body(str)
-    str[/int main\(\) \{(.*)\}/, 1]
+  def self.compile(src)
+    src = src.delete("\n")
+    src = src[/int main\(\) \{(.*)\}/, 1]
+    args = src.split(';')
+    args.each do |arg|
+      puts arg.chomp.strip
+    end
+    args
   end
 
   ##
