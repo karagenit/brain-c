31c28321f8fdba2b3484eb4b4ccbacd83a8a623b
Caleb Smith <karagenit@outlook.com>
2017-08-16 18:59:16 -0400

    Implement Object-Based Compiler

--- lib/brain_c.rb
+++ lib/brain_c.rb
@@ -2,18 +2,36 @@
 # Compilation tools for converting C to Brainfuck
 #
 module BrainC
-  def self.compile(src)
-    vars = {}
-
-    src = src.delete("\n")
-    src = src[/int main\(\) \{(.*)\}/, 1]
-    args = src.split(';')
-    args.each do |arg|
-      arg = arg.chomp.strip
-      vars[arg] = 1
-      puts arg
+  ##
+  # This class is used to compile C source to Brainfuck
+  #
+  class Compiler
+    def initialize(source)
+      raise ArgumentError if source.nil?
+      @src = source
+      @vars = {}
+      @out = ''
     end
-    args
+
+    ##
+    # Calls preprocess and then compile.
+    # Returns outputted Brainfuck
+    #
+    def exec
+      preprocess
+      @out
+    end
+
+    ##
+    # Used to convert raw source into a series of instructions.
+    # Returns a list of the instructions found.
+    #
+    def preprocess
+      @src = @src.delete("\n")
+      @src = @src[/int main\(\) \{(.*)\}/, 1]
+      @ins = @src.split(';')
+      @ins.map! { |inst| inst.chomp.strip }
+    end
   end
 
   ##
